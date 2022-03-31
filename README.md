a8070532e7f229689bd784c3ef9818807d764368
SherapLa <sherapgyaltsen@dcmail.ca>
2022-03-30 22:03:32 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Predictiong of fish species
+
+* Import all the packages and libraries.
+
+* Load the Fish.csv dataset using pandas dataframe.
+
+* Identify the datatypes of the features and target
+
+* Drop the assign target value for training and drop the Species column.
+
+* Encode the target label into numeric.
+
+* Plot graphs for analysis.
+
+* Build logistic regression model.
+
+* Fit the model for training.
+
+* Predict the model using Test datasets.
+
+* Check model accuracy which is 93.75%. Decent.
+
+* Show a metrix for training and testing set scores.
+
+* DUmp the model using pythonic pickle.
---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, url_for, redirect, render_template, jsonify
+#from pycaret.classification import load_model, predict_model
+import pandas as pd
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# with open('/Users/sherapgyaltsen/Desktop/fish_forecast/knn_model.pkl', 'rb') as savefile:
+#     model = pickle.load(savefile)
+# with open('/Users/sherapgyaltsen/Desktop/fish_forecast/knn_model.pkl', 'rb') as f:
+#     model = pickle.load(f)
+#
+#cols = ['Weight', 'Length1', 'Length2','Length3','Height','Width']
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	int_features = [x for x in request.form.values()]
+	final = np.array(int_features)
+	final = final.astype(np.float64)
+	#data_unseen = pd.DataFrame([final], columns=cols)
+	model = pickle.load(open('logreg.pkl','rb'))
+	prediction = model.predict([final])
+	print(prediction)
+	return render_template('home.html', prediction=prediction)
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+# 	int_features = [x for x in request.form.values()]
+# 	final = np.array(int_features)
+# 	data_unseen = pd.DataFrame([final], columns=cols)
+# 	prediction = predict_model(model, data = data_unseen, round=0)
+# 	prediction = int(prediction.Label[0])
+# 	return render_template('home.html', pred='Expected Fish Species will be {}'.format(prediction))
+
+if __name__ == '__main__':
+	app.run(debug=True)
